e3270159d8eeb0a8a1a964639f71f3c53c7884df
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-09-01 12:50:44 +0100

    fix: Error handling on deleting user book

--- app/src/main/java/com/example/chapterly/domain/use_case/DeleteUserBookUseCase.kt
+++ app/src/main/java/com/example/chapterly/domain/use_case/DeleteUserBookUseCase.kt
@@ -2,11 +2,14 @@ package com.example.chapterly.domain.use_case
 
 import com.example.chapterly.domain.model.BookEntry
 import com.example.chapterly.domain.repository.UserLibraryRepository
+import com.example.chapterly.resources.Error
+import com.example.chapterly.resources.Result
+
 
 class DeleteUserBookUseCase (
     private val userLibraryRepository: UserLibraryRepository
 ) {
-    suspend operator fun invoke(userBook: BookEntry){
-        userLibraryRepository.deleteUserBook(userBook)
+    suspend operator fun invoke(userBook: BookEntry): Result<Unit, Error>{
+        return userLibraryRepository.deleteUserBook(userBook)
     }
 }
