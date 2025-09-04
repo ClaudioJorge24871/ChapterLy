d0b09a15598ca44a08bd1db32a1cc7487ec80258
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-09-04 15:44:37 +0100

    style: Empty user library screen styled

--- app/src/main/java/com/example/chapterly/presentation/ui/user_library/UserLibraryScreen.kt
+++ app/src/main/java/com/example/chapterly/presentation/ui/user_library/UserLibraryScreen.kt
@@ -5,13 +5,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -20,11 +24,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.chapterly.domain.model.BookEntry
 import com.example.chapterly.presentation.mapper.toUIData
 import com.example.chapterly.resources.Result
+import org.intellij.lang.annotations.JdkConstants
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,16 +40,44 @@ fun UserLibraryScreen(viewModel: UserLibraryViewModel, innerPadding: PaddingValu
     val booksResult by viewModel.books.collectAsState()
 
     Scaffold(
-        topBar = { TopAppBar(title = { Text("Your Library") }) },
-        content = { innerPadding ->
-            Box(modifier = Modifier.padding(innerPadding)) {
+        topBar = { TopAppBar(title = { Text("Your Library") })},
+    ) {padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            Divider(
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
+                thickness = 1.dp
+            )
+            Box(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .padding(padding)
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
                 when (val result = booksResult) {
                     null -> {
                         Text("Loading books...")
                     }
                     is Result.Success -> {
                         if (result.data.isEmpty()){ // If the user doesn't have any books added
-                            Text(text = "Add books to your library")
+                            Column(
+                                verticalArrangement = Arrangement.spacedBy(10.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(
+                                    text = "It looks like you don't have any books saved.",
+                                    style = MaterialTheme.typography.titleMedium
+                                )
+                                Button(
+                                    onClick = {/* TODO navigate to add book screen */},
+                                ) {
+                                    Text(text = "Add books to your library")
+                                }
+                            }
                         }else{
                             BookList(
                                 books = result.data,
@@ -59,7 +93,7 @@ fun UserLibraryScreen(viewModel: UserLibraryViewModel, innerPadding: PaddingValu
                 }
             }
         }
-    )
+    }
 }
 
 @Composable
