d6ff175eceae5f980f04a4a949a2315840bad982
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-09-06 23:18:01 +0100

    feat: use case and use case injection to get user books by isbn ( GetUserBookByISBNUseCase.kt )

--- app/src/main/java/com/example/chapterly/di/UseCaseModule.kt
+++ app/src/main/java/com/example/chapterly/di/UseCaseModule.kt
@@ -3,6 +3,7 @@ package com.example.chapterly.di
 import com.example.chapterly.domain.repository.BookRepository
 import com.example.chapterly.domain.repository.UserLibraryRepository
 import com.example.chapterly.domain.use_case.DeleteUserBookUseCase
+import com.example.chapterly.domain.use_case.GetUserBookByISBNUseCase
 import com.example.chapterly.domain.use_case.GetUserBooksUseCase
 import com.example.chapterly.domain.use_case.SaveUserBookUseCase
 import dagger.Module
@@ -19,8 +20,12 @@ object UseCaseModule {
         GetUserBooksUseCase(repo)
 
     @Provides
-    fun provideSaveUserBookUseCase(UserLibraryRepo: UserLibraryRepository): SaveUserBookUseCase =
-        SaveUserBookUseCase(UserLibraryRepo)
+    fun provideGetUserBookByISBNUseCase(repo: UserLibraryRepository): GetUserBookByISBNUseCase =
+        GetUserBookByISBNUseCase(repo)
+
+    @Provides
+    fun provideSaveUserBookUseCase(userLibraryRepo: UserLibraryRepository): SaveUserBookUseCase =
+        SaveUserBookUseCase(userLibraryRepo)
 
     @Provides
     fun provideDeleteUserBookUseCase(repo: UserLibraryRepository): DeleteUserBookUseCase =
---
+++ app/src/main/java/com/example/chapterly/domain/use_case/GetUserBookByISBNUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.chapterly.domain.use_case
+
+import com.example.chapterly.domain.model.BookEntry
+import com.example.chapterly.domain.repository.UserLibraryRepository
+import javax.inject.Inject
+import com.example.chapterly.resources.Result
+import com.example.chapterly.resources.Error
+
+class GetUserBookByISBNUseCase @Inject constructor(
+    private val userLibraryRepository: UserLibraryRepository
+){
+    operator fun invoke(isbn: String): Result<BookEntry, Error> {
+        return userLibraryRepository.getBookByISBN(isbn)
+    }
+}
