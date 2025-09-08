715bdf6c891f851470ef569bfa600df4eb7f0ebb
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-09-08 23:00:28 +0100

    feat: Year is an number input with all digit and size verification

--- app/src/main/java/com/example/chapterly/presentation/ui/add_book/SaveBookScreen.kt
+++ app/src/main/java/com/example/chapterly/presentation/ui/add_book/SaveBookScreen.kt
@@ -1,6 +1,7 @@
 package com.example.chapterly.presentation.ui.add_book
 
 import android.graphics.Outline
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
@@ -33,6 +36,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.chapterly.domain.model.BookEntry
 import com.example.chapterly.presentation.dto.BookUIDataDTO
@@ -54,6 +59,8 @@ fun SaveBookScreen(
     initialBook: BookUIDataDTO = BookUIDataDTO(),
     isEdit: Boolean = false
 ) {
+    val context = LocalContext.current
+
     var book by remember { mutableStateOf(initialBook) }
 
     val saveEvent by viewModel.saveEvent.collectAsState(initial = null)
@@ -135,6 +142,7 @@ fun SaveBookScreen(
                 label = {Text(text = "ISBN")},
                 modifier = Modifier.fillMaxWidth(),
             )
+            Spacer(modifier = Modifier.height(12.dp))
             Row (
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.spacedBy(8.dp)
@@ -148,10 +156,19 @@ fun SaveBookScreen(
                 )
                 OutlinedTextField(
                     value = book.publishYear,
-                    onValueChange = { book = book.copy(publishYear = it) },
-                    label = { Text("Publish Year") },
-                    modifier = Modifier.weight(1f),
-                    singleLine = true
+                    onValueChange = { newValue ->
+                        if (newValue.all { it.isDigit() }) {
+                            val limitedValue = if (newValue.length > 4) {
+                                Toast.makeText(context, "Year cannot have more than 4 digits", Toast.LENGTH_SHORT).show()
+                                newValue.take(4)
+                            } else newValue
+                            book = book.copy(publishYear = limitedValue)
+                        }
+                    },
+                    label = { Text("Year") },
+                    modifier = Modifier.width(120.dp),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    singleLine = true,
                 )
             }
             Row(
@@ -168,7 +185,7 @@ fun SaveBookScreen(
                 OutlinedTextField(
                     value = book.pagination,
                     onValueChange = { book = book.copy(pagination = it) },
-                    label = { Text("Number of Pages") },
+                    label = { Text("Pagination") },
                     modifier = Modifier.weight(1f),
                     singleLine = true
                 )
