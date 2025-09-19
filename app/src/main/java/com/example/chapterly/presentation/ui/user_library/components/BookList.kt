5fac5fc3aba3878756851eb94cd3f6278693d96f
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-09-19 19:37:39 +0100

    style(BookCards): better UI cards with bookmark

--- app/src/main/java/com/example/chapterly/presentation/ui/user_library/components/BookList.kt
+++ app/src/main/java/com/example/chapterly/presentation/ui/user_library/components/BookList.kt
@@ -2,6 +2,7 @@ package com.example.chapterly.presentation.ui.user_library.components
 
 import android.R
 import android.adservices.topics.Topic
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -19,27 +21,35 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.chapterly.domain.model.BookEntry
 
 @Composable
 fun BookList(
     books: List<BookEntry>,
-    onSelectedBook: (Int) -> Unit
+    onSelectedBook: (Int) -> Unit,
+    onChangeStatus: (BookEntry) -> Unit
 ) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2), // Two books per row
@@ -49,46 +59,79 @@ fun BookList(
         horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         items(books) { bookEntry ->
-            Card(
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(160.dp),
-                onClick = {onSelectedBook(bookEntry.book.id)}
-            ){
-                Box(
+            ) {
+                ElevatedCard(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(16.dp),
+                        .height(160.dp),
+                    onClick = { onSelectedBook(bookEntry.book.id) },
+                    elevation = CardDefaults.cardElevation(8.dp)
                 ) {
-                    Column (
+                    Box(
                         modifier = Modifier
-                            .align(Alignment.TopStart)
                             .fillMaxWidth()
+                            .padding(16.dp),
                     ) {
-                        Text(
-                            text = bookEntry.book.title, style = MaterialTheme.typography.titleLarge,
-                            maxLines = 2, overflow = TextOverflow.Ellipsis,
-                        )
-                        Spacer(modifier = Modifier.weight(1f))
                         Column(
-                            modifier = Modifier.fillMaxHeight(),
-                            verticalArrangement = Arrangement.Bottom
+                            modifier = Modifier
+                                .align(Alignment.TopStart)
+                                .fillMaxWidth()
                         ) {
-                            Row(
+                            Text(
+                                text = bookEntry.book.title,
+                                style = MaterialTheme.typography.titleLarge,
+                                maxLines = 2,
+                                overflow = TextOverflow.Ellipsis,
+                            )
+                            Spacer(modifier = Modifier.weight(1f))
+                            Column(
+                                modifier = Modifier.fillMaxHeight(),
+                                verticalArrangement = Arrangement.Bottom
                             ) {
-                                Text(text= "by ", style = MaterialTheme.typography.titleSmall, color = Color.Gray)
+                                Row(
+                                ) {
+                                    Text(
+                                        text = "by ",
+                                        style = MaterialTheme.typography.titleSmall,
+                                        color = Color.Gray
+                                    )
+                                    Text(
+                                        text = bookEntry.book.author.toString(),
+                                        style = MaterialTheme.typography.titleMedium,
+                                        maxLines = 1,
+                                        overflow = TextOverflow.Ellipsis
+                                    )
+                                }
                                 Text(
-                                    text = bookEntry.book.author.toString(), style = MaterialTheme.typography.titleMedium,
-                                    maxLines = 1, overflow = TextOverflow.Ellipsis
+                                    "Status: ${bookEntry.status.displayName}",
+                                    style = MaterialTheme.typography.bodySmall
                                 )
                             }
-                            Text("Status: ${bookEntry.status.displayName}", style = MaterialTheme.typography.bodySmall)
                         }
+                        Spacer(modifier = Modifier.height(8.dp))
                     }
-                    Spacer(modifier = Modifier.height(8.dp))
+                }
+                IconButton(
+                    onClick = {onChangeStatus(bookEntry)},
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .offset(y = (20).dp, x = (5).dp)
+                        .background(
+                            color = MaterialTheme.colorScheme.primary,
+                            shape = RoundedCornerShape(50)
+                        )
+                        .size(40.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Bookmark,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
                 }
             }
         }
     }
-
 }
